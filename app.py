1ac0f09a320a2f6c5cee77ddd7927f2e697d0fa5
barcelosluiz2026 <sbmi1971@gmail.com>
2026-02-18 04:11:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -344,12 +344,3 @@ with app.app_context():
 
 if __name__ == "__main__":
     app.run()
-
-@app.route("/reset_database_123")
-def reset_database():
-    db.drop_all()
-    db.create_all()
-    return "Banco recriado com sucesso!"
-
-
-
